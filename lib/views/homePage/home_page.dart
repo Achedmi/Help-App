e9713b7ea209dda62e9418e7d5d2dcc49666ca4e
unknown <abdekrime.chedmi@gmail.com>
2022-01-26 16:23:17 +0100

    restructring

--- lib/views/homePage/home_page.dart
+++ lib/views/homePage/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:helpapp/Config/config.dart';
 import 'package:helpapp/Views/HomePage/Widgets/widgets.dart';
-import 'package:helpapp/views/homePage/widgets/home_page_body_balence.dart';
+import 'package:helpapp/views/homePage/widgets/home_page_body.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,14 +25,5 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class HomePageBody extends StatelessWidget {
-  const HomePageBody({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return HomePageBodyBalence();
   }
 }
---
+++ lib/views/homePage/widgets/home_page_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:helpapp/views/homePage/widgets/home_page_body_balence.dart';
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return HomePageBodyBalence();
+  }
+}
